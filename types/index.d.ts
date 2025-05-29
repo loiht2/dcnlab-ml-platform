cac5e6f964da310c4cf68eb3b22b24b8b84db925
Thanh Loi Hoang <loiht2@dcn.ssu.ac.kr>
2025-05-29 11:40:12 +0900

    centraldashboard kubeflow after modifying a little

---
+++ types/index.d.ts
@@ -0,0 +1,25 @@
+/**
+ * Types used in the server.
+ */
+
+declare namespace User {
+  export interface AuthObject {
+    [authHeader: string]: string;
+  }
+
+  export interface User {
+    email: string;
+    username: string;
+    domain: string;
+    hasAuth: boolean;
+    auth?: AuthObject;
+    userrole: string;
+  }
+}
+
+/** Extend the Express request with a User property */
+declare namespace Express {
+  export interface Request {
+    user?: User.User
+  }
+}
